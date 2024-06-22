8e458494f503d1ad1920439da70077a6ea72ed3f
AchmadWahyu <achwahyu.w@gmail.com>
2024-06-22 12:43:51 +0700

    fix: delete symbol need 3 times to delete

--- src/app/components/Calculator/Calculator.tsx
+++ src/app/components/Calculator/Calculator.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import Key from '../Key/Key';
 
@@ -7,7 +7,19 @@ import styles from './Calculator.module.scss';
 const Calculator = () => {
   const [panelValue, setPanelValue] = useState('');
 
-  const handleClick = () => {};
+  const isSymbol = useMemo(
+    () => panelValue[panelValue?.length - 1]?.trim().length === 0,
+    [panelValue]
+  );
+
+  const del = () => {
+    // if symbol, remove the symbol and 2 spaces (before and after the symbol)
+    if (isSymbol) {
+      setPanelValue(panelValue.substring(0, panelValue.length - 3));
+    } else {
+      setPanelValue(panelValue.substring(0, panelValue.length - 1));
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -22,12 +34,7 @@ const Calculator = () => {
           <Key onClick={() => setPanelValue(panelValue + '7')}>7</Key>
           <Key onClick={() => setPanelValue(panelValue + '8')}>8</Key>
           <Key onClick={() => setPanelValue(panelValue + '9')}>9</Key>
-          <Key
-            className={styles.key_text}
-            onClick={() =>
-              setPanelValue(panelValue.substring(0, panelValue.length - 1))
-            }
-          >
+          <Key className={styles.key_text} onClick={del}>
             DEL
           </Key>
 
@@ -49,9 +56,12 @@ const Calculator = () => {
           <Key className={styles.key_text} onClick={() => setPanelValue('')}>
             RESET
           </Key>
-          <Key className={styles.key_equal_sign} onClick={() => {
-            setPanelValue(String(eval?.(`"use strict"; ${panelValue}`)))
-          }}>
+          <Key
+            className={styles.key_equal_sign}
+            onClick={() => {
+              setPanelValue(String(eval?.(`"use strict"; ${panelValue}`)));
+            }}
+          >
             =
           </Key>
         </div>
